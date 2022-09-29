092d62aeb7d651f0352d902659d9410030c0fb01
Carol Santos <carolsvntos@gmail.com>
2022-09-29 19:27:32 -0300

    fixing js

--- dates.js
+++ dates.js
@@ -1,26 +1,49 @@
 (function(){
-    const today = new Date("4/3/2021");
-    const meeting = new Date("3/4/2019");
-    const seeing = new Date("3/17/2019");
-    const dating = new Date("4/3/2019");
+    const today = new Date();
+    const aniversarioMarco = new Date("04/15/1996");
+    const aniversarioCarol = new Date("07/13/1993");
+    const meeting = new Date("03/04/2019");
+    const seeing = new Date("03/17/2019");
+    const dating = new Date("04/03/2019");
+    const living = new Date("04/03/2021");
 
     const meetingPhrase = document.getElementById('meeting');
     const seeingPhrase = document.getElementById('seeing');
     const datingPhrase = document.getElementById('dating');
+    const livingPhrase = document.getElementById('living');
+
+    const idadeMarco = document.getElementById('idadeMarco');
+    const idadeCarol = document.getElementById('idadeCarol');
+    const yearsHero = document.getElementById('datingHero');
+
+    function yearComparison(date) {
+      const timeDiff = Math.abs(today.getTime() - date.getTime());
+      const fullDays = Math.floor(timeDiff / (1000 * 3600 * 24)); 
+      const years = Math.floor(fullDays / 365);
+
+      return `${years}`
+    }
+
+    idadeMarco.innerHTML += yearComparison(aniversarioMarco);
+    idadeCarol.innerHTML += yearComparison(aniversarioCarol);
+    yearsHero.innerHTML += yearComparison(dating);
 
     function dateCreator(date) {
         const timeDiff = Math.abs(today.getTime() - date.getTime());
-        const fullDays = Math.round(timeDiff / (1000 * 3600 * 24)); 
-        const years = Math.round(fullDays / 365);
-        const months = Math.round((fullDays % 365) / 30.5);
-        const days = Math.round(fullDays % 30.5)
+        const fullDays = Math.floor(timeDiff / (1000 * 3600 * 24)); 
+        const years = Math.floor(fullDays / 365);
+        const months = Math.floor((fullDays % 365) / 30.5);
+        const days = Math.floor((fullDays % 30.5) + 1);
 
-        return `${years} ano(s), ${months} mes(es) e ${days} dias(s)`
+        return `${years} ano${years > 1 ? 's' : ''}, 
+                ${months} mes${months > 1 ? 'es' : ''} e 
+                ${days} dia${days > 1 ? 's' : ''}`
     }
 
     meetingPhrase.innerHTML += dateCreator(meeting);
     seeingPhrase.innerHTML += dateCreator(seeing);
     datingPhrase.innerHTML += dateCreator(dating);
+    livingPhrase.innerHTML += dateCreator(living);
 
 })();
 
@@ -37,7 +60,6 @@
       var h = today.getHours();
       var m = today.getMinutes();
       var s = today.getSeconds();
-      // add a zero in front of numbers<10
       m = checkTime(m);
       s = checkTime(s);
       document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
